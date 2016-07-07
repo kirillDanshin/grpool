87aaabfc01eba0dfa7e81c54e5044fa8879c70ac
Kirill Danshin <k@guava.by>
2016-07-07 17:31:26 +0300

    move examples

--- examples/first/first.go
+++ examples/first/first.go
@@ -1,35 +1,32 @@
-package grpool
+package main
 
 import (
 	"fmt"
 	"runtime"
+	"time"
 
 	"github.com/ivpusic/grpool"
 )
 
-func second() {
+func first() {
 	numCPUs := runtime.NumCPU()
 	runtime.GOMAXPROCS(numCPUs)
 
 	// number of workers, and size of job queue
 	pool := grpool.NewPool(100, 50)
-	defer pool.Release()
 
-	// how many jobs we should wait
-	pool.WaitCount(10)
+	// release resources used by pool
+	defer pool.Release()
 
 	// submit one or more jobs to pool
 	for i := 0; i < 10; i++ {
 		count := i
 
 		pool.JobQueue <- func() {
-			// say that job is done, so we can know how many jobs are finished
-			defer pool.JobDone()
-
-			fmt.Printf("hello %d\n", count)
+			fmt.Printf("I am worker! Number %d\n", count)
 		}
 	}
 
-	// wait until we call JobDone for all jobs
-	pool.WaitAll()
+	// dummy wait until jobs are finished
+	time.Sleep(1 * time.Second)
 }
--- examples/second/second.go
+++ examples/second/second.go
@@ -1,32 +1,35 @@
-package grpool
+package main
 
 import (
 	"fmt"
 	"runtime"
-	"time"
 
 	"github.com/ivpusic/grpool"
 )
 
-func first() {
+func second() {
 	numCPUs := runtime.NumCPU()
 	runtime.GOMAXPROCS(numCPUs)
 
 	// number of workers, and size of job queue
 	pool := grpool.NewPool(100, 50)
-
-	// release resources used by pool
 	defer pool.Release()
+
+	// how many jobs we should wait
+	pool.WaitCount(10)
 
 	// submit one or more jobs to pool
 	for i := 0; i < 10; i++ {
 		count := i
 
 		pool.JobQueue <- func() {
-			fmt.Printf("I am worker! Number %d\n", count)
+			// say that job is done, so we can know how many jobs are finished
+			defer pool.JobDone()
+
+			fmt.Printf("hello %d\n", count)
 		}
 	}
 
-	// dummy wait until jobs are finished
-	time.Sleep(1 * time.Second)
+	// wait until we call JobDone for all jobs
+	pool.WaitAll()
 }
